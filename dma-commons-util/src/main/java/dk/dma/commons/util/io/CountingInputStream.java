a707905f2bf89d8647e717ec9ab753b3a06b2941
Kasper Nielsen <kasperni@gmail.com>
2013-01-23 17:28:53 +0100

    adding counting streams

---
+++ dma-commons-util/src/main/java/dk/dma/commons/util/io/CountingInputStream.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2008 Kasper Nielsen.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package dk.dma.commons.util.io;
+
+import static java.util.Objects.requireNonNull;
+
+import java.io.FilterInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * Wraps another input stream, counting the number of bytes written.
+ * 
+ * @author Kasper Nielsen
+ */
+public final class CountingInputStream extends FilterInputStream {
+
+    /** The counter. */
+    private final AtomicLong counter;
+
+    public CountingInputStream(InputStream in) {
+        this(in, new AtomicLong());
+    }
+
+    /**
+     * Wraps another input stream, counting the number of bytes read.
+     * 
+     * @param in
+     *            the input stream to be wrapped
+     */
+    public CountingInputStream(InputStream in, AtomicLong counter) {
+        super(in);
+        this.counter = requireNonNull(counter);
+    }
+
+    /** Returns the number of bytes read. */
+    public long getCount() {
+        return counter.get();
+    }
+
+    @Override
+    public int read() throws IOException {
+        int result = in.read();
+        if (result != -1) {
+            counter.incrementAndGet();
+        }
+        return result;
+    }
+
+    @Override
+    public int read(byte[] b, int off, int len) throws IOException {
+        int result = in.read(b, off, len);
+        if (result != -1) {
+            counter.addAndGet(result);
+        }
+        return result;
+    }
+}
---
+++ dma-commons-util/src/main/java/dk/dma/commons/util/io/CountingOutputStream.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) 2008 Kasper Nielsen.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package dk.dma.commons.util.io;
+
+import static java.util.Objects.requireNonNull;
+
+import java.io.FilterOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * Wraps another output stream, counting the number of bytes written.
+ * 
+ * @author Kasper Nielsen
+ */
+public final class CountingOutputStream extends FilterOutputStream {
+
+    /** The counter. */
+    private final AtomicLong counter;
+
+    /**
+     * Wraps another output stream, counting the number of bytes written.
+     * 
+     * @param out
+     *            the output stream to be wrapped
+     */
+    public CountingOutputStream(OutputStream out) {
+        this(out, new AtomicLong());
+    }
+
+    /**
+     * Wraps another output stream, counting the number of bytes written.
+     * 
+     * @param out
+     *            the output stream to be wrapped
+     * @param counter
+     *            the atomic long that will be incremented
+     */
+    public CountingOutputStream(OutputStream out, AtomicLong counter) {
+        super(out);
+        this.counter = requireNonNull(counter);
+    }
+
+    /** Returns the number of bytes written. */
+    public long getCount() {
+        return counter.get();
+    }
+
+    @Override
+    public void write(byte[] b, int off, int len) throws IOException {
+        out.write(b, off, len);
+        counter.addAndGet(len);
+    }
+
+    @Override
+    public void write(int b) throws IOException {
+        out.write(b);
+        counter.incrementAndGet();
+    }
+}
