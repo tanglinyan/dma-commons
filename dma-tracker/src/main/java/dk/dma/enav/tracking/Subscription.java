df67d2386daa33ff463aefccca57c28fc2566e3e
Kasper Nielsen <kasperni@gmail.com>
2013-01-23 11:15:16 +0100

    update subscription

--- dma-tracker/src/main/java/dk/dma/enav/tracking/Subscription.java
+++ dma-tracker/src/main/java/dk/dma/enav/tracking/Subscription.java
@@ -22,10 +22,12 @@ import java.util.Map;
 
 import jsr166e.ConcurrentHashMapV8;
 import jsr166e.ConcurrentHashMapV8.Action;
+import jsr166e.ConcurrentHashMapV8.BiAction;
+import dk.dma.app.util.function.BiBlock;
 import dk.dma.app.util.function.Block;
+import dk.dma.enav.model.geometry.Area;
 import dk.dma.enav.model.geometry.Position;
 import dk.dma.enav.model.geometry.PositionTime;
-import dk.dma.enav.model.geometry.Area;
 
 /**
  * A subscription is created for each {@link PositionUpdateHandler}. It is to use unsubscribe ({@link #cancel()}).
@@ -34,11 +36,8 @@ import dk.dma.enav.model.geometry.Area;
  */
 public class Subscription<T> {
 
-    /** A map of currently tracked objects for this subscription. */
-    private final ConcurrentHashMapV8<T, PositionTime> trackedObjects = new ConcurrentHashMapV8<>();
-
-    /** The tracker that this subscription is registered with. */
-    private final PositionTracker<T> tracker;
+    /** The handler that should be called whenever objects are entering/exiting. */
+    private final PositionUpdateHandler<? super T> handler;
 
     /** The shape we look at to see if we are entering the area of interest. */
     private final Area shapeEntering;
@@ -46,8 +45,11 @@ public class Subscription<T> {
     /** The shape we look at to see if we are exiting the area of interest. */
     private final Area shapeExiting;
 
-    /** The handler that should be called whenever objects are entering/exiting. */
-    private final PositionUpdateHandler<? super T> handler;
+    /** A map of currently tracked objects for this subscription. */
+    private final ConcurrentHashMapV8<T, PositionTime> trackedObjects = new ConcurrentHashMapV8<>();
+
+    /** The tracker that this subscription is registered with. */
+    private final PositionTracker<T> tracker;
 
     Subscription(PositionTracker<T> tracker, PositionUpdateHandler<? super T> handler, Area shape, Area exitShape) {
         this.tracker = requireNonNull(tracker);
@@ -61,6 +63,66 @@ public class Subscription<T> {
         if (tracker.subscriptions.remove(handler, this)) {
             trackedObjects.clear();
         }
+    }
+
+    /**
+     * Performs the given block for each tracked object in parallel.
+     * 
+     * @param block
+     *            the block
+     */
+    public void forEachTrackedObject(final Block<T> block) {
+        requireNonNull(block, "block is null");
+        trackedObjects.forEachKeyInParallel(new Action<T>() {
+            @Override
+            public void apply(T t) {
+                block.accept(t);
+            }
+        });
+    }
+
+    /**
+     * Performs the given block for each tracked object in parallel.
+     * 
+     * @param block
+     *            the block
+     */
+    public void forEachTrackedObject(final BiBlock<T, Position> block) {
+        requireNonNull(block, "block is null");
+        trackedObjects.forEachInParallel(new BiAction<T, PositionTime>() {
+            @Override
+            public void apply(T t, PositionTime pt) {
+                // pt.time is from the first time we encountered the position.
+                // We might have gotten messages with the position but different timestamps
+                // To avoid confusion we do not export the timestamp out
+                block.accept(t, Position.create(pt.getLatitude(), pt.getLongitude()));
+            }
+        });
+    }
+
+    /**
+     * Returns the number of tracked objects.
+     * 
+     * @return the number of tracked objects
+     */
+    public int getNumberOfTrackedObjects() {
+        return trackedObjects.size();
+    }
+
+    /**
+     * Returns a map of tracked objects with their current position.
+     * 
+     * @return a map of tracked objects with their current position
+     */
+    public Map<T, Position> getTrackedObjects() {
+        // We could return trackedObjects directly. But we do not want to return PositionTime objects
+        // because the time is not the time from the latest report. But the first time with the current position.
+        // Which would be easily to mistake for users.
+        HashMap<T, Position> result = new HashMap<>();
+        for (Map.Entry<T, PositionTime> e : trackedObjects.entrySet()) {
+            result.put(e.getKey(), Position.create(e.getValue().getLatitude(), e.getValue().getLongitude()));
+        }
+        return result;
     }
 
     /**
@@ -92,46 +154,4 @@ public class Subscription<T> {
             }
         }
     }
-
-    /**
-     * Returns a map of tracked objects with their current position.
-     * 
-     * @return a map of tracked objects with their current position
-     */
-    public Map<T, Position> getTrackedObjects() {
-        // We could result trackedObjects directly. But we do not want to return PositionTime objects
-        // because the time is not the latest report. But the first time with the current position.
-        // Which would be easily to mistake for users.
-        HashMap<T, Position> result = new HashMap<>();
-        for (Map.Entry<T, PositionTime> e : trackedObjects.entrySet()) {
-            result.put(e.getKey(), Position.create(e.getValue().getLatitude(), e.getValue().getLongitude()));
-        }
-        return result;
-    }
-
-    /**
-     * Returns the number of tracked objects.
-     * 
-     * @return the number of tracked objects
-     */
-    public int getNumberOfTrackedObjects() {
-        return trackedObjects.size();
-    }
-
-    /**
-     * Performs the given block for each tracked object in parallel.
-     * 
-     * @param block
-     *            the block
-     */
-    public void forEachTrackedObject(final Block<T> block) {
-        requireNonNull(block, "block is null");
-        trackedObjects.forEachKeyInParallel(new Action<T>() {
-            @Override
-            public void apply(T t) {
-                block.accept(t);
-            }
-        });
-    }
-
 }
