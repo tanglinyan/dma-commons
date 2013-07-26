46a1f2eec7c559a3621b9619cb7b0a99b8eb84ae
Kasper Nielsen <kasperni@gmail.com>
2013-07-26 09:28:39 +0200

    adding date time util

---
+++ dma-commons-management/src/main/java/dk/dma/commons/management/ManagedComposite.java
@@ -0,0 +1,41 @@
+/* Copyright (c) 2011 Danish Maritime Authority
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package dk.dma.commons.management;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 
+ * @author Kasper Nielsen
+ */
+
+public abstract class ManagedComposite {
+
+    @Target(ElementType.TYPE)
+    @Inherited
+    @Retention(RetentionPolicy.RUNTIME)
+    public @interface Annotation {
+
+    }
+
+    public void addToThis(Object o) {
+
+    }
+}
